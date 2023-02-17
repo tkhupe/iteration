b5676af1f229ec944e00ee2b57f1a9b8e4cda35b
Tshakalisa Khupe <88752230+tkhupe@users.noreply.github.com>
2023-02-17 02:38:20 -0600

    completed

---
+++ script.js
@@ -0,0 +1,8 @@
+//WRITE YOUR CODE BELOW
+var students = ['jo', 'mo', 'bo', 'tj', 'li']
+console.log(students.length);
+
+for (i = 0; i < students.length; i++) {
+
+    console.log('Great to see you, ' + students[i] + '!')
+}
